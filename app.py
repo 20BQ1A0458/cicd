ef5a11d757abbf9fe1d36556692ed4a395cb08ac
Bhargav Ram <163117308+20BQ1A0458@users.noreply.github.com>
2024-11-10 15:58:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # app.py
 def greet():
-    return "Hello, World! my name is G.Bhargav Ram Iam working in Qcffjgfdlair"
+    return "Hello, World! my name is fdlair"
 
 if __name__ == "__main__":
     print(greet())
