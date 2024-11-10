edfe71c187355574ac771ebf5e7d7fc444232599
Bhargav Ram <163117308+20BQ1A0458@users.noreply.github.com>
2024-11-10 16:20:48 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -4,11 +4,7 @@ from app import greet
 
 class TestApp(unittest.TestCase):
     def test_greet(self):
-<<<<<<< HEAD
-        self.assertEqual(greet(), "Hello, World! my name is Bhargav ram")
-=======
-        self.assertEqual(greet(), "Hello, World! World! my name is fdlair")
->>>>>>> 4f821eaf5a35756dc8d0e1b421e253333b9e2f52
+        self.assertEqual(greet(), "Hello, World! my name  Bhargav ram")
 
 if __name__ == "__main__":
     unittest.main()
